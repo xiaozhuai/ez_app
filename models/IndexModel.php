41abb3abcac75e02f8ef10fe47fc6e07dfcfc7b1
xiaozhuai <798047000@qq.com>
2016-12-20 07:38:09 +0800

    move setTable argument to __construct function

--- models/IndexModel.php
+++ models/IndexModel.php
@@ -10,8 +10,7 @@ class IndexModel extends EZSql
 {
 
     function __construct(){
-        $this->setTable("PKU");            //set table used by this model, must be called before "parent::__construct()"
-        parent::__construct();
+        parent::__construct("PKU");
     }
 
     public function test(){
