9b2d4279d1ba1eef7881b9662fb2cec0ca408a8e
xiaozhuai <798047000@qq.com>
2016-12-20 01:44:04 +0800

    add pdo config options

--- conf/db.config.php
+++ conf/db.config.php
@@ -8,5 +8,8 @@
 return array(
     "DB_DSN"                          => "mysql:host=localhost;port=3306;dbname=ez_pdo",
     "DB_USER"                         => "root",
-    "DB_PWD"                          => "root"
+    "DB_PWD"                          => "root",
+    "DB_OPTIONS"                      => array(
+                                             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                                         )
 );
