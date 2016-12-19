4188042d01f4ae4d1ae90edfa3eecba05eadfcb4
xiaozhuai <798047000@qq.com>
2016-12-20 07:33:21 +0800

    rename class EZModel to EZSql

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -9,63 +9,12 @@
 class IndexController extends EZController
 {
     public function index(){
-//        EZ()->loadModel("/Index");
-//        $model = new IndexModel();
         $this->getView()->name = "EZer";
         $this->getView()->view_engine = EZConfig()->VIEW_ENGINE;
         $this->getView()->render();
-//        echo "<br><hr><br>";
-//        var_dump($model->insert(
-//            [
-//                "name" => "aaa"
-//            ]
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->insert(
-//            [
-//                "name" => "aaa'"
-//            ]
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->insert(
-//            [
-//                "name" => "aaa\""
-//            ]
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->insert(
-//            [
-//                "name" => "bbb\\"
-//            ]
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->insert(
-//            [
-//                "name" => "ccc"
-//            ]
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->getAll());
-//        echo "<br><hr><br>";
-//        var_dump($model->update(
-//            [
-//                "name" => "ccc_new"
-//            ],
-//            sprintf("`name`='ccc'")
-//        ));
-//        echo "<br><hr><br>";
-//        var_dump($model->getAll());
-//        echo "<br><hr><br>";
-//        var_dump($model->del(sprintf("`name`='aaa'")));
-//        echo "<br><hr><br>";
-//        var_dump($model->getAll());
-//        echo "<br><hr><br>";
-//        var_dump($model->getOneRow());
-//        echo "<br><hr><br>";
-//        var_dump($model->getAllRowsOneField("`name`"));
-//        echo "<br><hr><br>";
-//        var_dump($model->getOneRowOneField("`name`"));
-
+//        EZ()->loadModel("/Index");
+//        $model = new IndexModel();
+//        $model->test();
     }
 
 }
--- models/IndexModel.php
+++ models/IndexModel.php
@@ -6,12 +6,66 @@
  * Date: 16/12/20
  * Time: 上午1:50
  */
-class IndexModel extends EZModel
+class IndexModel extends EZSql
 {
 
     function __construct(){
         $this->setTable("PKU");            //set table used by this model, must be called before "parent::__construct()"
         parent::__construct();
+    }
+
+    public function test(){
+        echo "<br><hr><br>";
+        var_dump($this->insert(
+            [
+                "name" => "aaa"
+            ]
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->insert(
+            [
+                "name" => "aaa'"
+            ]
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->insert(
+            [
+                "name" => "aaa\""
+            ]
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->insert(
+            [
+                "name" => "bbb\\"
+            ]
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->insert(
+            [
+                "name" => "ccc"
+            ]
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->getAll());
+        echo "<br><hr><br>";
+        var_dump($this->update(
+            [
+                "name" => "ccc_new"
+            ],
+            sprintf("`name`='ccc'")
+        ));
+        echo "<br><hr><br>";
+        var_dump($this->getAll());
+        echo "<br><hr><br>";
+        var_dump($this->del(sprintf("`name`='aaa'")));
+        echo "<br><hr><br>";
+        var_dump($this->getAll());
+        echo "<br><hr><br>";
+        var_dump($this->getOneRow());
+        echo "<br><hr><br>";
+        var_dump($this->getAllRowsOneField("`name`"));
+        echo "<br><hr><br>";
+        var_dump($this->getOneRowOneField("`name`"));
     }
 
 }
